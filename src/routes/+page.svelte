1ac2d5f29f77c73b247fda0ef1cdffbdfd4090d4
Aritra Mondal <aritra041999.dev@gmail.com>
2023-05-16 21:03:31 +0200

    Added RoutesList component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import explorerJson from '$lib/data/routes.json';
+    import { buildPaths } from '$lib/utils/utils';
+
+    const paths = buildPaths(explorerJson.paths);
+</script>
+
+
+
+
+
