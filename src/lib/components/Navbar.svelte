87a7545dd003787bac84ac53ae0c51b7a7723639
Aritra Mondal <aritra041999.dev@gmail.com>
2023-05-17 17:05:56 +0200

    Wrapped Route items in links

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,13 +1,15 @@
-<nav class="fixed flex items-center justify-between w-screen h-20">
-    <div>
-        Routes
-    </div>
-    <div class="flex items-center space-x-4">
-        <div>
-            Login
+<nav class="fixed w-screen bg-blue-600 text-white">
+    <div class="container mx-auto flex items-center justify-between h-20 ">
+        <div class="font-black">
+            /routes 
         </div>
-        <div>
-            
+        <div class="flex items-center space-x-4 font-medium">
+            <div>
+                Login
+            </div>
+            <div>
+                
+            </div>
         </div>
     </div>
 </nav>
