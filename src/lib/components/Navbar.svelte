daf9ea889eac46ab29cdb91e6ca03945a7eac563
Aritra Mondal <aritra041999.dev@gmail.com>
2023-05-17 15:51:04 +0200

    Added new navbar component

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<nav class="fixed flex items-center justify-between w-screen h-20">
+    <div>
+        Routes
+    </div>
+    <div class="flex items-center space-x-4">
+        <div>
+            Login
+        </div>
+        <div>
+            
+        </div>
+    </div>
+</nav>
