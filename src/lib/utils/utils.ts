89d638f7cb59f02876af0e5812a0ea74eede2607
Aritra Mondal <aritra041999.dev@gmail.com>
2023-05-17 15:50:03 +0200

    Modified types and route list builder to hold more properties

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -4,13 +4,22 @@ export function buildPaths(paths: Record<string, any>): MetaRoute[] {
     const routes: MetaRoute[] = [];
 
     for(const [key, value] of Object.entries(paths)) {
-        for(const [method, path] of Object.entries(value)) {
+        for(const [method, route] of Object.entries(value)) {
+            const path = route as Record<string, any>;
             routes.push({
-                name: path.summary,
                 path: key,
-                method: method.toUpperCase() as 'GET' | 'POST' | 'PUT' | 'DELETE'
+                tags: path.tags,
+                name: path.summary,
+                security: path.security,
+                response: path.responses,
+                operationId: path.operationId,
+                method: method.toUpperCase() as 'GET' | 'POST' | 'PUT' | 'DELETE',
             })
         }
     }
     return routes;
+}
+
+export function getRouteColorClass(method: string): string {
+    return `${method.toLowerCase()}-tag`;
 }
