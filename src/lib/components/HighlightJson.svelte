3de74927e86ae504942d0bef28b8643c73e9f8ba
aritra2021 <95604384+aritra2021@users.noreply.github.com>
2023-05-20 22:23:28 +0200

    Added HighLight Component for auto highlighting json code

---
+++ src/lib/components/HighlightJson.svelte
@@ -0,0 +1,14 @@
+<script lang=ts>
+    import hljs from 'highlight.js';
+    import json from "highlight.js/lib/languages/json";
+    // import 'highlight.js/styles/github.css';
+    import 'highlight.js/styles/atom-one-light.css';
+
+    export let code: Record<string, any>;
+
+    hljs.registerLanguage('json', json);
+    const highlightedCode = hljs.highlight(JSON.stringify(code, null, 2), {language: 'json'}).value
+
+</script>
+
+<pre><code>{@html highlightedCode}</code></pre>
