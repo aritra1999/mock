477a2663b5a603a429f9b3d49f2dd9dfec97f879
aritra2021 <95604384+aritra2021@users.noreply.github.com>
2023-05-20 22:25:45 +0200

    Added methods for parsing schemas from reference objects

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -25,46 +25,34 @@ export function getRouteColorClass(method: string): string {
 	return `${method.toLowerCase()}-tag`;
 }
 
-export function buildRequestSchema(requestSchema: Record<string, any>): Record<string, any> {
-	let parsedRequestSchema: Record<string, string[][]> = {};
-	for (const [format, schema] of Object.entries(requestSchema)) {
-		console.log(format, schema);
-		if (schema['schema'].hasOwnProperty('$ref')) {
-			const refPath: string[] = schema['schema']['$ref'].split('/');
-			return buildSchemaFromRef(refPath[3]);
-		} else {
-			console.log('no ref');
-		}
-		parsedRequestSchema[format] = [['hello', 'world']];
+export function parseSchema(schema: Record<string, any>): Record<string, any> {
+	
+	const parsedSchema: Record<string, any> = {};
+
+	if (schema['$ref']) {
+		const refPath: string[] = schema['$ref'].split('/');
+		return buildSchemaFromRef(refPath[3]);
+	} else {
+		console.log('no ref');
 	}
 
-	return {
-		'application/json': [
-			['hello', 'world'],
-			['this', 'test']
-		]
-	};
+
+	return parsedSchema;
 }
 
 function buildSchemaFromRef(schemaName: string) {
 	const components: Record<string, any> = routesJson.components;
 	const schema = components.schemas[schemaName];
 
-	if (!schema) {
-		throw new Error(`Schema '${schemaName}' not found in components.`);
-	}
+	if (!schema) throw new Error(`Schema '${schemaName}' not found in components.`);
 
 	const result = { ...schema };
 
-	// Check if the schema has properties
 	if (schema.properties) {
 		result.properties = {};
-
 		for (const propName in schema.properties) {
-			if (schema.properties.hasOwnProperty(propName)) {
+			if (schema.properties[propName]) {
 				const propSchema = schema.properties[propName];
-
-				// If the property has a $ref, recursively build the referenced schema
 				if (propSchema.$ref) {
 					const refSchemaName = propSchema.$ref.replace('#/components/schemas/', '');
 					result.properties[propName] = buildSchemaFromRef(refSchemaName);
