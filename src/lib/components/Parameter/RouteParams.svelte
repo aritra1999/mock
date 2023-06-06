a4a16b88aa8b63a0598bd191c2ef589ed8c98d18
Aritra Mondal <aritra041999.dev@gmail.com>
2023-06-06 07:54:58 +0200

    RouteParamter - Restructure

--- src/lib/components/Parameter/RouteParams.svelte
+++ src/lib/components/Parameter/RouteParams.svelte
@@ -1,8 +1,9 @@
-<script lang="ts">   
-    export let parameters: Record<string, any>[]; 
+<script lang="ts">
+    import type { Parameter } from "$lib/utils/types";
+	import { getParameterFieldNames } from "./utils";
 
-    const parameterFields = ["name", "description", "properties"];
-
+    export let parameters: Parameter[]; 
+    const parameterFields = getParameterFieldNames(parameters);
 </script>
 
 
@@ -10,22 +11,15 @@
     <table class="w-full text-left text-gray-500">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
-                <th scope="col" class="px-4 py-2">
-                    Name
-                </th>
-                <th scope="col" class="px-4 py-2">
-                    Description
-                </th>
-                <th scope="col" class="px-4 py-2">
-                    Properties
-                </th>
-                <th scope="col" class="px-4 py-2">
-                    Enum
-                </th>
+                {#each parameterFields as paramField}
+                    <th scope="col" class="px-4 py-2">
+                        {paramField}
+                    </th>
+                {/each}
             </tr>
         </thead>
         <tbody class="text-xs">
-            {#each parameters as parameter}
+            {#each parameters as parameter}                           
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <td class="px-4 py-2 ">
                         <div class="text-sm font-semibold text-slate-700"> 
@@ -39,7 +33,9 @@
                         </div>
                     </td>
                     <td class="px-4 py-2">
-                        {parameter.description}
+                        {#if parameter.description}
+                            {parameter.description}
+                        {/if}
                     </td>
                     <td class="px-4 py-2">
                         {#if parameter.required}
@@ -57,10 +53,3 @@
         </tbody>
     </table>
 </div>
-
-<div class="flex flex-col space-y-2">
-    
-        <!-- <Parameter parameter={parameter} /> -->
-    
-</div>
-
